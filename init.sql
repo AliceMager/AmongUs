6b44f792f77e4462560434f42bc58ebced93d8cb
alice <alicemager006@gmail.com>
2026-03-04 17:40:28 +0200

    changed the status type to be an enum of status

--- init.sql
+++ init.sql
@@ -1,9 +1,14 @@
 create schema amongus;
 
+create type status as enum (
+  'CREATED',
+  'DELETED'
+);
+
 create table amongus.deployment(
     id UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
     db_name VARCHAR NOT null,
-    status VARCHAR NOT null,
+    status status NOT null,
     username VARCHAR NOT null,
     creation_time timestamp without time zone not null default (current_timestamp at time zone 'utc')
 );
