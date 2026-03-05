738da52a96a5530a0261cb11fd67044397627dfa
alice <alicemager006@gmail.com>
2026-03-05 17:30:08 +0200

    modified the file to create the tables from models, removed the logger

--- postgres.py
+++ postgres.py
@@ -1,19 +1,6 @@
-from enum import Enum
-from sqlalchemy import Column, String, DateTime, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from models import Base
 
-SQLALCHEMY_DB_URL = 'postgresql://postgres:postgres@localhost:5432/amongus_db'
-engine = create_engine(SQLALCHEMY_DB_URL)
-
-Session = sessionmaker(bind=engine)
-Session = Session()
-
-Base = declarative_base()
-
-def get_db():
-    db = Session()
-    try:
-        yield db
-    finally:
-        db.close()
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/amongus_db"
+engine = create_engine(DATABASE_URL, echo=True)
+Base.metadata.create_all(engine)
