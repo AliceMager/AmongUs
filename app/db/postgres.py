9778383ddbdc71c925219aabff86b07a297261e9
alice <alicemager006@gmail.com>
2026-03-06 13:20:02 +0200

    added a Session

--- app/db/postgres.py
+++ app/db/postgres.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
-from models import Base
+from sqlalchemy.orm import sessionmaker
+
+from app.tables.deployments import Base
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/amongus_db"
 engine = create_engine(DATABASE_URL, echo=True)
 Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
