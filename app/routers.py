401edea5436eab025c925cbfa48fb19f4fb6132c
alice <alicemager006@gmail.com>
2026-03-06 12:24:54 +0200

    implemented a creation route to add the metadata to the postgres db

--- app/routers.py
+++ app/routers.py
@@ -1,7 +1,18 @@
+import datetime
+import uuid
 from fastapi import APIRouter
 from app.models.deployment import Deployment
+from app.db.postgres import Session
+from app.tables.deployments import Deployments, Status
 router = APIRouter()
 
 @router.post("/deployments", tags=["users"])
 async def create_deployment(deployment: Deployment):
-    ...
+    session = Session()
+    dep_id = uuid.uuid4()
+    new_deployment = Deployments(id=dep_id,db_name=deployment.db_name,
+                    status=Status.CREATED,username=deployment.username, creation_time=datetime.datetime.now())
+    session.add(new_deployment)
+    session.commit()
+    session.close()
+    return dep_id
